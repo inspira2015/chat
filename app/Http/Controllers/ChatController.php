ebaca391d483a83fefcd3340e41bd2a0a3fb399e
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-09-26 11:03:33 -0400

    Added Pusher

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Events\ChatEvent;
 
 class ChatController extends Controller
 {
@@ -20,10 +21,11 @@ class ChatController extends Controller
 		return view('chat');
 	}
 
-	public function send(Request $request)
+	public function send()
 	{
+		$message = "Hello World";
 		$user = User::find(Auth::id());
-		event(new ChatEvent($request->message, $user));
+		event(new ChatEvent($message, $user));
 	}
 
 }
